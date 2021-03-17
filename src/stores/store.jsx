12039c4a5ebb9739f1d9b8715d7f251c6a7535d3
nick <morri2ne@dukes.jmu.edu>
2021-03-17 15:42:33 -0400

    added 'a' and 'fee' parameters to deploy_metapool call

--- src/stores/store.jsx
+++ src/stores/store.jsx
@@ -793,7 +793,7 @@ class Store {
   _callDeployMetapool = async (web3, account, basePool, address, name, symbol, a, fee, callback) => {
     const curveFactoryContract = new web3.eth.Contract(config.curveFactoryV2ABI, config.curveFactoryV2Address)
 
-    curveFactoryContract.methods.deploy_metapool(basePool.erc20address, name, symbol, address, '10', '4000000').send({ from: account.address, gasPrice: web3.utils.toWei(await this._getGasPrice(), 'gwei') })
+    curveFactoryContract.methods.deploy_metapool(basePool.erc20address, name, symbol, address, a, fee).send({ from: account.address, gasPrice: web3.utils.toWei(await this._getGasPrice(), 'gwei') })
     .on('transactionHash', function(hash){
       emitter.emit(SNACKBAR_TRANSACTION_HASH, hash)
       callback(null, hash)
