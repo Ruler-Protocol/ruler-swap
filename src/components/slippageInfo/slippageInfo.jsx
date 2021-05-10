30ab0140cc5736b06ffe856fda3df9fd9f0578d6
MickNorris <lorem.ipsum.crypto@gmail.com>
2021-05-10 12:54:46 -0400

    removed notice from slippage info

--- src/components/slippageInfo/slippageInfo.jsx
+++ src/components/slippageInfo/slippageInfo.jsx
@@ -40,8 +40,6 @@ const SlippageInfo = ({ slippagePcent, classes }) => {
 
       {' '}(incl. pricing): <strong>{formattedSlippagePcent}</strong>
 
-      {isVeryHighSlippage &&
-        <Fragment><br />(Calculated assuming a value of 1 for all assets)</Fragment>}
     </Alert>
   )
 }
